53a2219557b8d59aaad13c86a79fe053e4cae810
vbertazzo <vbertazzo@gmail.com>
2021-06-12 16:38:56 -0300

    build: remove unnecessary files

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-screen flex flex-col justify-center items-center"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({})
+</script>
